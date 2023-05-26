f65fa7b585de51ed78b3dfe89e64232c413ddb96
SalavatSM <salavatfromkg@gmail.com>
2023-05-26 17:16:59 +0600

    Add create.html, add ProductCreateForm and ReviewCreateForm. HW_5

---
+++ products/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class ProductCreateForm(forms.Form):
+    image = forms.FileField(required=False)
+    title = forms.CharField(max_length=256)
+    description = forms.CharField(widget=forms.Textarea())
+    rate = forms.FloatField()
+
+
+class ReviewCreateForm(forms.Form):
+    text = forms.CharField(max_length=256)
+
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-from products.models import Product
+
+from products.models import Product, Review
+from products.forms import ProductCreateForm, ReviewCreateForm
 
 # Create your views here.
 
@@ -29,6 +31,58 @@ def product_detail_view(request, id):
         }
 
         return render(request, 'products/detail.html', context=context)
+
+
+def product_create_view(request):
+    if request.method == 'GET':
+
+        context = {
+            'form': ProductCreateForm
+        }
+
+        return render(request, 'products/create.html', context=context)
+
+    if request.method == 'POST':
+        data, files = request.POST, request.FILES
+        form = ProductCreateForm(data, files)
+
+        if form.is_valid():
+            Product.objects.create(
+                image=form.cleaned_data.get('image'),
+                title=form.cleaned_data.get('title'),
+                description=form.cleaned_data.get('description'),
+                rate=form.cleaned_data.get('rate'),
+            )
+            return redirect('/products/')
+
+        return render(request, 'products/create.html', context={
+            'form': form
+        })
+
+
+def review_create_view(request):
+    if request.method == 'GET':
+
+        context = {
+            'form': ReviewCreateForm
+        }
+
+        return render(request, 'products/detail.html', context=context)
+
+    if request.method == 'POST':
+        data, files = request.POST, request.FILES
+        form = ReviewCreateForm(data, files)
+
+        if form.is_valid():
+            Review.objects.create(
+                # image=form.cleaned_data.get('image'),
+                text=form.cleaned_data.get('text')
+            )
+            return redirect('/review/')
+
+        return render(request, 'products/detail.html', context={
+            'form': form
+        })
 
 
 
