47f3bfd88d905a52c2c0f0600b99d1429f296c80
SalavatSM <salavatfromkg@gmail.com>
2023-05-29 11:48:40 +0600

    Add RegisterForm, LoginForm. Add register.html, login.html. Add logic of authorization

--- HW_Project/urls.py
+++ HW_Project/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 from products.views import main_page_view, products_view, product_detail_view, product_create_view, review_create_view
 from HW_Project import settings
 
+from users.views import login_view, register_view, logout_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page_view),
@@ -12,6 +14,9 @@ urlpatterns = [
     path('products/create/', product_create_view),
     path('review/create/', review_create_view),
     path('products/<int:id>/', product_detail_view),
+    path('users/login/', login_view),
+    path('users/register/', register_view),
+    path('users/logout/', logout_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
