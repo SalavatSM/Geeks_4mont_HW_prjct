bc480f6966a631a40f3ebd0abf4d4851979029af
SalavatSM <salavatfromkg@gmail.com>
2023-06-02 14:50:27 +0600

    Add product search func by title and description. Add constants.py

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 
 from products.models import Product, Review
 from products.forms import ProductCreateForm, ReviewCreateForm
+from products.constants import PAGINATION_LIMIT
 
 # Create your views here.
 
@@ -14,10 +15,26 @@ def main_page_view(request):
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+        max_page = products.__len__() / PAGINATION_LIMIT
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products[PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
+
+        if search:
+            products = products.filter(title__icontains=search)
+
+        if search:
+            products = products.filter(description__icontains=search)
 
         context = {
             'products': products,
-            'user': request.user
+            'user': request.user,
+            'pages': range(1, max_page+1)
         }
         return render(request, 'products/products.html', context=context)
 
